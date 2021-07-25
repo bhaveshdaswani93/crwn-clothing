4e1c442ca631dbf65a29377e5c49a6e7e1b60c37
Bhavesh Daswani <bhaveshdaswani93@gmail.com>
2021-07-25 23:33:07 +0530

    added Checkout Item component with feature to update cart

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -4,10 +4,21 @@ export const addItemToCart = (currentItems,itemToAdd) =>{
     if(item) {
         return currentItems.map(currentItem=>{
             if(currentItem.id === item.id) {
-                currentItem.quantity = currentItem.quantity + 1
+              return {...currentItem,  quantity:currentItem.quantity + 1}
             }
             return currentItem;
         })
     }
     return [...currentItems,{...itemToAdd,quantity:1}];
+}
+
+export const removeItemFromCart  = (currentItems,itemToRemove) => {
+    const currentItem = currentItems.find(currentItem=>currentItem.id === itemToRemove.id);
+
+    if(currentItem.quantity === 1) {
+        return currentItems.filter(item=>currentItem.id !== item.id)
+    }
+    return currentItems.map(item=>{
+        return  item.id === currentItem.id ? {...currentItem,quantity:currentItem.quantity - 1}:item
+    });
 }
