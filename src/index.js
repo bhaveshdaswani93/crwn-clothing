cca81079138d444a356f2b136cfc96c67bdbf548
Bhavesh Daswani <bhaveshdaswani93@gmail.com>
2021-07-31 19:15:22 +0530

    Added stripe library

--- src/index.js
+++ src/index.js
@@ -6,15 +6,21 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
+import {Elements} from '@stripe/react-stripe-js';
+import {loadStripe} from '@stripe/stripe-js';
 
 import {store,persistor} from './redux/store'
+const stripePromise = loadStripe('pk_test_51JJHAsSCUIDynQcDq4hpDv6FYSVo4wfXGaEu3KZrZe7tFRsafD4fJ81J1SlmTQjIBqy0yujaDbZYLGqtW4pg3Ey900UmVkI2Ff');
+
 
 ReactDOM.render(
   <Provider store={store}>
     <React.StrictMode>
       <BrowserRouter>
       <PersistGate persistor={persistor}>
-        <App />
+        <Elements stripe={stripePromise}>
+          <App />
+        </Elements>
       </PersistGate>
       </BrowserRouter>
     </React.StrictMode>
--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -6,6 +6,7 @@ import './checkout.styles.scss'
 import CheckoutItem from '../../components/checkout-item/checkout-item.component'
 
 import {selectCartItems,selectCartItemsTotal} from  '../../redux/cart/cart.selector'
+import CheckoutForm from '../../components/stripe-element/stripe-element.component'
 
 const CheckoutPage = ({cartItems,total})=>(
     <div className='checkout-page'>
@@ -30,6 +31,13 @@ const CheckoutPage = ({cartItems,total})=>(
         <div className='total'>
             <span>${total}</span>
         </div>
+        <div className='test-warning'>
+            *Please use following test credit card for payments*
+            <br />
+            4242 4242 4242 4242 - Exp: 12/25 - CVV: 123 - ZIP: 12345
+        </div>
+
+        <CheckoutForm />
 
     </div>
 )
